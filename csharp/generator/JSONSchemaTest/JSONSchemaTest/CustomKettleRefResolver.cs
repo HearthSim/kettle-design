775b8563f3b0894f6d215bdefbbfa2c172a944c1
Bert Proesmans <bproesmans@hotmail.com>
2017-06-23 23:12:01 +0200

    Added test project for C# code generation from schemas

---
+++ csharp/generator/JSONSchemaTest/JSONSchemaTest/CustomKettleRefResolver.cs
@@ -0,0 +1,45 @@
+﻿using NJsonSchema;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JSONSchemaTest
+{
+    class CustomKettleRefResolver : JsonReferenceResolver
+    {
+        private readonly string _kettleRootSchemaPath;
+        private readonly Dictionary<string, Task<JsonSchema4>> _loadedSchemas;
+
+        public CustomKettleRefResolver(JsonSchemaResolver schemaResolver, string kettleRootSchemaPath) : base(schemaResolver)
+        {
+            _kettleRootSchemaPath = kettleRootSchemaPath;
+            _loadedSchemas = new Dictionary<string, Task<JsonSchema4>>();
+        }
+
+        public override async Task<JsonSchema4> ResolveFileReferenceAsync(string filePath)
+        {
+            var kettlePrefix = "kettle:";
+            var kettleIdx = filePath.IndexOf(kettlePrefix);
+            if (kettleIdx != -1)
+            {
+                // Change filepath to properly identify schemas referenced by the kettle IRI.
+                var kettlePath = filePath.Substring(kettleIdx + kettlePrefix.Length);
+                filePath = Path.Combine(_kettleRootSchemaPath, kettlePath + ".json");
+            }
+
+            if (_loadedSchemas.ContainsKey(filePath))
+            {
+                // Wait for parsing to finish!
+                return await _loadedSchemas[filePath].ConfigureAwait(false);
+            }
+
+            var loadTask = JsonSchema4.FromFileAsync(filePath, schema => this);
+            _loadedSchemas[filePath] = loadTask;
+            return await loadTask.ConfigureAwait(false);
+        }
+    }
+}
---
+++ csharp/generator/JSONSchemaTest/JSONSchemaTest/CustomKettleTypeNameResolver.cs
@@ -0,0 +1,36 @@
+﻿using NJsonSchema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONSchemaTest
+{
+    class CustomKettleTypeNameResolver : DefaultTypeNameGenerator
+    {
+        public override string Generate(JsonSchema4 schema, string typeNameHint, IEnumerable<string> reservedTypeNames)
+        {
+            var title = schema.Title;
+
+            if (title == null || title.Length == 0)
+            {
+                title = base.Generate(schema, typeNameHint, reservedTypeNames);
+            }
+            else
+            {
+                if (reservedTypeNames.Contains(title))
+                {
+                    var count = 1;
+                    do
+                    {
+                        count++;
+                    } while (reservedTypeNames.Contains(title + count));
+                    title = title + count;
+                }
+            }
+
+            return title;
+        }
+    }
+}
